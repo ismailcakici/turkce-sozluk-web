2a3b432d1c9365e91a5b4b9ebe9e91d6e2ac4268
İsmail Çakıcı <ismailcakici30@gmail.com>
2024-10-16 17:37:02 +0300

    sign language function added

---
+++ src/utils/SignLanguage/SignLanguage.ts
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { baseUrl } from "../../constants/BaseURL/BaseURL";
+
+export const signLanguage = async (letter:string) : Promise<string> => {
+    try {
+        if (letter.length !== 1) {
+            throw new Error("Letter count must be 1.")
+        }
+        const response = await axios.get(`${baseUrl}/assets/img/isaret/${letter}.gif`);
+        return response.data;
+    } catch (error) {
+        console.log("An error occured while fetching sign language data.")
+        throw new Error("Error while fetching gif.");
+    }
+}
