5f8c33d78feb98e6a8271a3cb96cd6b120a67b7b
İsmail Çakıcı <ismailcakici30@gmail.com>
2024-10-18 15:39:34 +0300

    search word function added

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,23 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
+import autoCompleteData from "../../constants/AutoComplete/AutoComplete.json";
 
 interface SearchProps {
   word: string;
+  searchWord: (word: string) => void;
   setWord: (newWord: string) => void;
 }
 
-const Search: React.FC<SearchProps> = ({ word, setWord }) => {
+interface AutoCompleteItem {
+  madde: string;
+}
+
+const Search: React.FC<SearchProps> = ({ word, setWord, searchWord }) => {
+  const autoCompleteList: AutoCompleteItem[] =
+    autoCompleteData as AutoCompleteItem[];
+  const [filteredSuggestions, setFilteredSuggestions] = useState<string[]>([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const userInput = e.target.value;
+    setWord(userInput);
+
+    if (userInput.length >= 3) {
+      const suggestions = autoCompleteList
+        .filter((item) =>
+          item.madde.toLowerCase().startsWith(userInput.toLowerCase())
+        )
+        .map((item) => item.madde);
+
+      setFilteredSuggestions(suggestions);
+      setShowSuggestions(true);
+    } else {
+      setShowSuggestions(false);
+    }
+  };
+
+  const handleSuggestionClick = (suggestion: string) => {
+    setWord(suggestion);
+    setShowSuggestions(false);
+  };
+
   return (
-    <label className="input input-bordered rounded-box flex items-center text-xs md:text-sm lg:text-base gap-2 my-3">
-      <input
-        type="text"
-        className="grow"
-        value={word}
-        onChange={(e) => setWord(e.target.value)}
-        placeholder="Güncel Türkçe Sözlük'te Ara"
-      />
-      <FaSearch />
-    </label>
+    <div>
+      <label className="input input-bordered rounded-box flex items-center text-xs md:text-sm lg:text-base my-3">
+        <input
+          type="text"
+          className="grow"
+          placeholder="Güncel Türkçe Sözlük'te Ara"
+          value={word}
+          onChange={handleInputChange}
+        />
+        <button
+          onClick={() => searchWord(word)}
+          className="btn absolute btn-success rounded-box right-5"
+        >
+          <FaSearch />
+        </button>
+      </label>
+
+      {showSuggestions && (
+        <ul className="absolute bg-base-100 border rounded-box shadow-lg w-[90%] sm:w-[95%] max-h-80 overflow-auto z-10">
+          {filteredSuggestions.length > 0 ? (
+            filteredSuggestions.map((suggestion, idx) => (
+              <li
+                key={idx}
+                className="p-2 cursor-pointer hover:bg-base-200"
+                onClick={() => handleSuggestionClick(suggestion)}
+              >
+                {suggestion}
+              </li>
+            ))
+          ) : (
+            <li className="p-2 text-gray-500">Sonuç bulunamadı</li>
+          )}
+        </ul>
+      )}
+    </div>
   );
 };
 
--- src/layout/main/Main.tsx
+++ src/layout/main/Main.tsx
@@ -2,17 +2,31 @@ import React, { useState } from "react";
 import Header from "../../components/Header/Header";
 import Search from "../../components/Search/Search";
 import SpecialCharList from "../../components/SpecialCharList/SpecialCharList";
+import { WordResponse } from "../../types/Word/WordResponse";
+import { fetchWordData } from "../../service/SearchWord/SearchWord";
 
 const Main: React.FC = () => {
   const [word, setWord] = useState("");
+  const [wordData, setWordData] = useState<WordResponse[]>();
+  const [loading, setLoading] = useState(false);
 
   const handleSpecialCharClick = (char: string) =>
     setWord((prevWord) => prevWord + char);
 
+  const searchWord = async (word: string) => {
+    setLoading(true);
+    try {
+      const response = await fetchWordData(word);
+      setWordData(response);
+    } catch (error) {
+      console.error("Error occured while fetching word data", error);
+    }
+    setLoading(false);
+  };
   return (
     <div className="w-full lg:w-4/6 bg-base-300 drop-shadow-lg min-h-screen rounded-box transform transition-all p-5">
       <Header />
-      <Search word={word} setWord={setWord} />
+      <Search word={word} setWord={setWord} searchWord={searchWord} />
       <SpecialCharList onClick={handleSpecialCharClick} />
     </div>
   );
